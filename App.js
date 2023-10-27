fabd7ad0fe99bed4374df9a5f139ccf50114810a
nhatlinh <nhatlinhdev201@gmail.com>
2023-10-27 07:30:57 +0700

    final screen1

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen1 from './src/Screen1';
+import Screen2 from './src/Screen2';
+import Screen3 from './src/Screen3';
+import Screen4 from './src/Screen4';
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Screen1' component={Screen1} options={{headerShown: false}}/>
+        <Stack.Screen name='Screen2' component={Screen2}/>
+        <Stack.Screen name='Screen3' component={Screen3}/>
+        <Stack.Screen name='Screen4' component={Screen4}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/Screen1.js
+++ src/Screen1.js
@@ -1,10 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Screen1 = () => {
+const Screen1 = ({navigation, route}) => {
   return (
-    <View>
-      <Text>Screen1</Text>
+    <View style={{
+        flex : 1,
+        alignItems : 'center'
+    }}>
+      <Text style={{
+        fontWeight : 700,
+        fontSize : 25,
+        margin : 30
+      }}>Welcome to Cafe World</Text>
+      <Image 
+        source={require('../Data/image5.jpeg')}
+        style={{
+            width : 250,
+            height : 100,
+            borderRadius : 8,
+            margin : 30
+        }}
+      />
+      <Image 
+        source={require('../Data/image5.jpeg')}
+        style={{
+            width : 250,
+            height : 100,
+            borderRadius : 8,
+            margin : 30
+        }}
+      />
+      <Image 
+        source={require('../Data/image5.jpeg')}
+        style={{
+            width : 250,
+            height : 100,
+            borderRadius : 8,
+            margin : 30
+        }}
+      />
+
+      <Pressable
+      onPress={()=> navigation.navigate('Screen2')}
+        style={{
+            width : '90%',
+            height : 50,
+            backgroundColor : 'blue',
+            justifyContent:'center',
+            alignItems : 'center',
+            borderRadius : 8
+        }}
+      >
+        <Text style={{
+            color : 'white',
+            fontSize : 20,
+            fontWeight : 700
+        }}>GET STATED</Text>
+      </Pressable>
+
     </View>
   )
 }
